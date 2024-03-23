c71a0188eb992073a6eb0286cf0cd04a40aed650
Hajime Yagihara <eitiwai@gmail.com>
2024-03-23 12:38:35 +0900

    [fix] norm to RMSNorm

--- mybitnet/bitnet/bitnet.py
+++ mybitnet/bitnet/bitnet.py
@@ -1,6 +1,24 @@
 import torch
 from torch import nn
 
+class BitRMSNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        BitRMSNorm is equivalent to LlamaRMSNorm and T5LayerNorm
+        refers: https://github.com/huggingface/transformers/blob/c5f0288bc7d76f65996586f79f69fba8867a0e67/src/transformers/models/llama/modeling_llama.py#L76C1-L90C59
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        return self.weight * hidden_states.to(input_dtype)
+    
+
 class BitLinear(nn.Linear):
     def __init__(self, in_features, out_features, bias=False, bits=8, flg_before_linear=True):
         super(BitLinear, self).__init__(in_features, out_features, bias)
